40db3f423ceb9ea0505e25c3ba18fa0d8868aadc
Your Name <you@example.com>
2023-01-29 01:13:53 +0300

    Zadacha 6

---
+++ Zadacha006/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.Write("Введите трёхзначное число: ");
+double a = double.Parse(Console.ReadLine()!);
+
+if (100 <= a && a <= 999)    // Проверка на трёхзначность!
+{
+    System.Console.WriteLine($"Последняя цифра числа {a} это {a % 10}");
+    System.Console.WriteLine();
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное!");
+}
