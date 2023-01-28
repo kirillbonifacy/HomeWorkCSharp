9cad8abbdd8d2772a5916d6d3551110db50a5326
Your Name <you@example.com>
2023-01-29 00:45:52 +0300

    Zadacha 4

---
+++ Zadacha004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+Console.WriteLine("Введите число для проверки чётности: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
