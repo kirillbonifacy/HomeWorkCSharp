47b1681060c24df571e5d01d3bb55363235e147e
Your Name <you@example.com>
2023-01-29 01:42:16 +0300

    Zadacha 8

---
+++ Zadacha008/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int a = new Random().Next(10, 100);
+int max = 0;
+
+int maxDigit()
+{
+    int DigitOne = a / 10;
+    int DigitTwo = a % 10;
+    if (DigitOne > DigitTwo) max = DigitOne;
+    else max = DigitTwo;
+
+    return max;
+}
+
+maxDigit();
+
+Console.WriteLine($"В числе {a} наибольшая цифра {max}");
