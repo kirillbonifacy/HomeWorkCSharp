6a44eeab24c06fedcab75d64e4908c036563e12f
Your Name <you@example.com>
2023-01-29 00:40:24 +0300

    Zadacha3

---
+++ Zadacha003/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти максимальное из трех чисел
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+
+if ( b > max)
+{
+    max = b;
+}
+
+if ( c > max )
+{
+    max = c;
+}
+Console.WriteLine("Наибольшее число: " + max);
