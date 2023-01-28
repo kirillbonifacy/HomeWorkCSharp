6968f9028afa3286b4e5cb7ecc74438749f56ef1
Your Name <you@example.com>
2023-01-29 00:49:35 +0300

    Zadacha 5

---
+++ Zadacha005/Program.cs
@@ -0,0 +1,22 @@
+﻿// Показать четные числа от 1 до N
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+int i = 1;
+bool no = true;
+
+Console.Write($"Чётные числа от 1 до {a}: ");
+while (i <= a)
+{
+    if (i % 2 != 1)
+    {
+        Console.Write(i + ", ");
+        no = false;
+    }
+i = i + 1;
+}
+
+if (no == true)
+{
+    Console.WriteLine("Нет чётных чисел!");
+}
